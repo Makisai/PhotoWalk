734b8a2ddb8612ca38c7886c6cbc3dec105a99b9
Makisai <jschmaglowski@aol.com>
2021-01-12 01:40:00 +0100

    add leaflet map for photowalks

---
+++ Frontend/src/components/PhotowalkMap.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="map">
+    <l-map
+        :zoom="zoom"
+        :center="center"
+        :options="mapOptions"
+        style="height: 80%"
+        @update:center="centerUpdate"
+    >
+      <l-tile-layer
+        :url="url"
+        :attribution="attribution"
+      />
+      <l-marker :lat-lng="currentCenter" ></l-marker>
+      <l-polyline :lat-lngs="polyline.latlngs" :color="polyline.color"></l-polyline>
+    </l-map>
+    <v-btn @click="addPoint">add position</v-btn>
+  </div>
+</template>
+
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LMarker, LPolyline } from 'vue2-leaflet';
+
+export default {
+  name: 'PhotowalkMap',
+  components: {
+    LMap,
+    LTileLayer,
+    LPolyline,
+    LMarker,
+  },
+  data() {
+    return {
+      zoom: 17,
+      center: latLng(53.56923, 10.034911),
+      currentCenter: latLng(53.56923, 10.034911),
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+          '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      mapOptions: {
+        zoomSnap: 0.5
+      },
+      polyline: {
+        latlngs: [],
+        color: 'green'
+      }
+    };
+  },
+  methods: {
+    centerUpdate(center) {
+      this.currentCenter = center;
+    },
+    // pure debugging method
+    addPoint() {
+      this.polyline.latlngs.push([this.currentCenter.lat, this.currentCenter.lng]);
+      console.log('[',this.polyline.latlngs.join('],['),']');
+    },
+  }
+};
+</script>
+
+<style scoped>
+.map {
+  height: 700px;
+  width: 30%;
+  z-index: 1;
+}
+</style>
