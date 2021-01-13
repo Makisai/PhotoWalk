5b9bcafc9cc48ef1cb57db4f53f8bfc26e469e24
Makisai <jschmaglowski@aol.com>
2021-01-13 22:24:48 +0100

    add files for Database Deployment

--- Frontend/src/plugins/axios.js
+++ Frontend/src/plugins/axios.js
@@ -9,7 +9,7 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  baseURL: process.env.baseURL || process.env.apiUrl || 'http:\\\\localhost:8080/api'
+  baseURL: process.env.VUE_APP_BACKEND_URL || process.env.baseURL || process.env.apiUrl || 'http:\\\\localhost:8080/api'
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
