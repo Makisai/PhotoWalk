ed35ba165675a3d28959fad59365da3daa85b500
Mike10101 <mike10101@t-online.de>
2021-01-15 18:33:48 +0100

    versuch babel zu benutzen

--- Frontend/src/views/Start.vue
+++ Frontend/src/views/Start.vue
@@ -1,8 +1,15 @@
 <template>
 <!--vuetify div-->
   <v-container>
-    <p>WILLKOMMEN NACH LOGIN/REGISTRIERUNG</p>
-    <NavigationButton v-bind:title="'Back'" v-bind:target="'LandingPage'"/>
+    <p v-if="!lastPhotowalk">{{$t(welcome)}}</p>
+    <p v-if="lastPhotowalk"> {{$t(welcomeback)}}</p>
+    <NavigationButton v-bind:title="'Back'" v-bind:target="'LandingPage'"/> 
+    <v-container>
+       <div class="div1" v-if="!lastPhotowalk">
+       </div>
+       <div class="div1" v-if="lastPhotowalk">
+       </div>    
+    </v-container>
     <PhotoUpload/>
   </v-container>
 </template>
@@ -15,3 +22,12 @@ export default {
   components: {PhotoUpload, NavigationButton},
 }
 </script>
+
+<style scoped>
+.div1 { 
+    margin: "10px";
+    height: 400px;
+    background-color: "blue";
+}
+
+</style>
