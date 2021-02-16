7269c5a1ed4f194a10eed80b7b6823f32feb4f7e
Michelle <michelleweck@web.de>
2021-02-16 19:57:49 +0100

    add feedback after search
    Signed-off-by: Michelle <michelleweck@web.de>

--- Frontend/src/components/friendsmanagement/SearchNewFriend.vue
+++ Frontend/src/components/friendsmanagement/SearchNewFriend.vue
@@ -2,16 +2,17 @@
   <v-container>
     <v-text-field
         style="width: 40%"
-        class="mx-auto card"
-        hint="Seach for new friends!"
+        class="mx-auto card ma-2"
+        hint="Search for new friends!"
         persistent-hint
         clearable
         v-model="input"
         append-icon="mdi-magnify"
         @click:append="search"
     ></v-text-field>
+    <p v-text="feedback"/>
     <v-card style="width: 400px"
-            class="mx-auto card ma-3" v-if="$store.state.search.foundUser !== null">
+            class="mx-auto card" v-if="$store.state.search.foundUser !== null">
       <v-list one-line>
         <v-list-item>
           <v-list-item-avatar>
@@ -36,6 +37,11 @@ import {SET_SEARCH_USERNAME, SET_FOUND_USER, SET_FRIENDS_LIST} from "@/store/mut
 
 export default {
   name: "SearchNewFriend",
+  data() {
+    return {
+      feedback: '',
+    }
+  },
   beforeDestroy() {
     this.$store.commit(SET_SEARCH_USERNAME,'');
     this.$store.commit(SET_FOUND_USER, null);
@@ -52,13 +58,33 @@ export default {
   },
   methods:{
     search() {
-      this.axios.get(`users/search/${this.$store.state.search.inputUsername}`,{
-        headers: {
-          'Authorization': `Bearer ${this.$store.state.user.token}`
+      if (this.$store.state.search.inputUsername !== this.$store.state.user.username) {
+        let friends = new Array();
+        for (let i = 0; i< this.$store.state.user.friends.friends.length; i++) {
+          friends.push(this.$store.state.user.friends.friends[i].username);
         }
-      }).then(response => {
-        this.$store.commit(SET_FOUND_USER,response.data);
-      })
+        if (!friends.includes(this.$store.state.search.inputUsername)) {
+          this.axios.get(`users/search/${this.$store.state.search.inputUsername}`,{
+            headers: {
+              'Authorization': `Bearer ${this.$store.state.user.token}`
+            }
+          }).then(response => {
+            if (!response.data) {
+              this.feedback = 'Nobody found...';
+            } else {
+              this.feedback = '';
+            }
+            this.$store.commit(SET_FOUND_USER,response.data);
+          })
+        } else {
+          this.$store.commit(SET_FOUND_USER, null);
+          this.feedback = 'You\'re already friends with this user!';
+        }
+      } else {
+        this.$store.commit(SET_FOUND_USER, null);
+        this.feedback = 'You can\'t be friends with yourself!';
+
+      }
     },
     profilePicture(picture){
       return process.env.VUE_APP_PUBLIC_URL + picture;
