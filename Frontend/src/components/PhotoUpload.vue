82c374a79cd146f58a2db1042682be2bf26188e9
Mike10101 <mike10101@t-online.de>
2021-03-07 18:01:26 +0100

    Error & Success Handling on Component Photoupload functional.

--- Frontend/src/components/PhotoUpload.vue
+++ Frontend/src/components/PhotoUpload.vue
@@ -1,38 +1,90 @@
  <template>
    <div>
+      <v-col v-if="internalError">
+        <p>{{$t('error.internalError')}} </p>
+      </v-col>
+        <v-col v-if="incompleteError">
+        <p>{{$t('error.incompleteError')}} </p>
+      </v-col>
+      <v-col class="py-2" cols="12">
       <v-file-input
-          label="File input"
-          filled
-          prepend-icon="mdi-camera"
-          v-model="upload"
+        :rules="[rules.notEmpty,rules.mimetype]"
+        label="File input"
+        filled
+        prepend-icon="mdi-camera"
+        v-model="upload"
       ></v-file-input>
+      </v-col>
+      <v-col> 
+        <select v-model="challengeId">
+          <option disabled value="">{{$t('photoUpload.selectChallengeId')}}</option>
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+        </select>
+      </v-col>
      <v-btn @click="submitUpload">{{ $t('labels.submit') }}</v-btn>
+    <v-col v-if="uploaded">
+      <p>{{$t('success.photoUpload')}} </p>
+    </v-col>
    </div>
  </template>
 
 <script>
-export default {
-  name: 'PhotoUpload',
-  data() {
-    return {
-      upload: null,
-    }
-  },
-  methods: {
-    submitUpload(){
-      let formData = new FormData();
-      formData.append('photo_link',this.upload);
-      formData.append('challengeId',this.$store.state.detail.selectedChallenge.id);
-      this.axios.post('photos/', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          'Authorization': `Bearer ${this.$store.state.user.token}`
-        }
-      })
-    }
+  export default {
+    name: 'PhotoUpload',
+    data() {
+      return {
+        upload: null,
+        challengeId: '',
+        uploaded: false,
+        internalError: false,
+        incompleteError: false,
+        rules: {
+          notEmpty: v => !!v ||'file is required',
+          mimetype: v => {
+            const pattern = /.*(\.png|\.jpg|\.jpeg){1}$/mg
+            return pattern.test(v.name)  || 'Invalid mimetype'
+          },
+        },
+      }
+    },
+    methods: {
+      submitUpload(){ 
+        let formData = new FormData();
+        formData.append('photo_link',this.upload);
+        formData.append('challengeId',this.challengeId);
+        this.axios.post('photos/', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            'Authorization': `Bearer ${this.$store.state.user.token}`
+          }
+        }).then((response) => {
+            if(response.status == 201){
+              this.uploaded = true;
+              this.incompleteError = false;
+              this.internalError = false;
+            } 
+          })
+          .catch((error) => {  
+            if(error.response && error.response.status == 400){
+              this.incompleteError = true;
+              this.internalError = false;
+            }
+            if(error.response && error.response.status == 500){
+              this.internalError = true;
+              this.incompleteError = false;
+            }
+          })
+      }
 
+    }
   }
-}
 </script>
 
 <style scoped>
