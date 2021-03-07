3d9152a313e28a5f7d7db5c0b61c3262b2d1ee85
Makisai <jschmaglowski@aol.com>
2021-03-07 22:09:54 +0100

    add image for walk1 in overview

--- Frontend/src/views/WalksOverview.vue
+++ Frontend/src/views/WalksOverview.vue
@@ -3,8 +3,17 @@
     <p>Chose a walk</p>
     <v-row>
       <v-col cols="4">
-        <v-card @click="goToDetail(1)">
-          walk1
+        <v-card @click="goToDetail(1), walkDone=true">
+          <v-img v-if="!walkDone"
+            :src="`/images/map_stadtpark_notDone_small.png`"
+            :lazy-src="`images/gradient.png`"
+            class = "rounded-lg">
+          </v-img>
+          <v-img v-if="walkDone"
+             :src="`/images/map_stadtpark_Done_small.png`"
+             :lazy-src="`images/gradient.png`"
+             class = "rounded-lg">
+          </v-img>
         </v-card>
       </v-col>
       <v-col cols="4">
@@ -26,6 +35,22 @@ import {SET_CURRENT_ID} from "@/store/mutations";
 
 export default {
   name: 'WalksOverview',
+  data(){
+    return{
+      walkDone: false,
+    }
+  },
+  beforeMount() {
+    this.axios.get(`photowalks/`,{
+      headers: {
+        'Authorization': `Bearer ${this.$store.state.user.token}`
+      }
+    }).then(response =>{
+      console.log(response)
+      // eslint-disable-next-line no-debugger
+      debugger
+    })
+  },
   methods:{
     goToDetail(walkNumber){
       this.$store.commit(SET_CURRENT_ID,walkNumber)
