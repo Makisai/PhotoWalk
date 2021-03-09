cbcd1835e426af4af64fdba0391b613f998973b8
Mike10101 <mike10101@t-online.de>
2021-03-09 16:23:56 +0100

    Settings Profile Picture with Error & Success handling is functional

--- Frontend/src/components/usermanagement/SettingsProfilePicture.vue
+++ Frontend/src/components/usermanagement/SettingsProfilePicture.vue
@@ -2,6 +2,12 @@
   <div> 
     <h1>{{($t('settings.profile'))}} </h1>
     <v-row class="row1">
+      <v-col v-if="incompleteError" cols="12">
+        <p class ="error">{{$t('error.incompleteError')}}</p>
+      </v-col>
+      <v-col v-if="internalError" cols="12">
+        <p class ="error">{{$t('error.internaleError')}}</p>
+      </v-col>
       <v-col cols="4">
         <p>{{ $t('settings.changeProfilePicture') }} <br> {{ $t('settings.advice') }}</p>
       </v-col>
@@ -14,6 +20,7 @@
       </v-col>
       <v-col cols="5">
         <v-file-input
+            :rules="[rules.required,rules.mimetype,rules.size]"
             filled
             name="profile-picture"
             prepend-icon="mdi-camera"
@@ -21,6 +28,9 @@
         ></v-file-input>
         <v-btn @click="updatePhoto">{{ $t('labels.submit') }}</v-btn>
       </v-col>
+      <v-col v-if="updated">
+      <p>{{$t('success.profilePictureUpdated')}} </p>
+    </v-col>
     </v-row>
   </div>
 </template>
@@ -37,6 +47,17 @@ export default {
   data() {
     return {
       updateProfilePicture: null,
+      incompleteError: false,
+      internalError: false,
+      updated: false,
+      rules: {
+          notEmpty: v => !!v ||'file is required',
+          size: v => v.size <= 1024*1024* 35 || 'filesize too big',
+          mimetype: v => {
+            const pattern = /.*(\.png|\.jpg|\.jpeg){1}$/mg
+            return pattern.test(v.name)  || 'Invalid mimetype'
+          },
+      },
     }
   },
   methods: {
@@ -49,10 +70,23 @@ export default {
           'Authorization': `Bearer ${this.$store.state.user.token}`
         }
       }).then((response) => {
-        this.$store.commit(SET_PROFILEPICTURE, response.data.path);
+          if(response.status == 200){
+            this.updated = true;
+            this.internalError = false;
+            this.incompleteError = false;
+            this.$store.commit(SET_PROFILEPICTURE, response.data.path);
+          }  
       }).catch((error) => {
-        this.error = 'error.profilePicture'
-        console.log("FEHLER", error);
+          if(error.response && error.response.status == 400){
+            this.incompleteError = true;
+            this.internalError = false;
+            this.updated = false;
+            }
+          if(error.response && error.response.status == 500){
+            this.internalError = true;
+            this.incompleteError = false;
+            this.updated = false;
+            }
       })
     },
   }
