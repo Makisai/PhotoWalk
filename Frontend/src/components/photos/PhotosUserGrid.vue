8f925fe56cae4e681a0b14c73967bb7f7e873784
Makisai <jschmaglowski@aol.com>
2021-03-10 12:34:21 +0100

    add scss file for further styling

--- Frontend/src/components/photos/PhotosUserGrid.vue
+++ Frontend/src/components/photos/PhotosUserGrid.vue
@@ -1,20 +1,31 @@
 <template>
-  <!--vuetify div-->
   <v-container v-if="walkPhotos.length > 0">
     <v-row align="center" class="ma-5">
       <v-divider></v-divider><h3 class="font-weight-regular">WALK {{walk}}</h3><v-divider></v-divider>
     </v-row>
     <div v-masonry item-selector=".item" class="masonry-container">
-      <div v-masonry-tile class="item pa-2" :key="index" v-for="(photo, index) in walkPhotos">
+      <div v-masonry-tile
+           class="item pa-2"
+           :key="index"
+           v-for="(photo, index) in walkPhotos">
         <v-card max-width="400px">
           <v-dialog @click:outside="onDialogClose">
             <template v-slot:activator="{ on, attrs }" >
-              <v-img max-height="500px" class="align-end flex-md-wrap" @load="imageLoaded"
-                     :src="picture(photo.photo_link)" @click="dialog = true" v-bind="attrs" v-on="on">
+              <v-img
+                  max-height="500px"
+                  class="align-end flex-md-wrap"
+                  @load="imageLoaded"
+                  :src="picture(photo.photo_link)"
+                  @click="dialog = true"
+                  v-bind="attrs" v-on="on">
               </v-img>
             </template>
             <v-card>
-              <PhotosUserCarousel v-if="dialog" :start-index="index" :walk="walk"></PhotosUserCarousel>
+              <PhotosUserCarousel
+                  v-if="dialog"
+                  :start-index="index"
+                  :walk="walk">
+              </PhotosUserCarousel>
             </v-card>
           </v-dialog>
           <v-card-actions style="max-height: 45px">
@@ -22,7 +33,11 @@
             <v-spacer></v-spacer>
             <v-card-text>{{photo.likeCount}}
               <v-btn icon>
-                <v-icon :color="photo.liked ? 'red' : 'grey'" @click=like(index,1)>mdi-heart</v-icon>
+                <v-icon
+                    :color="photo.liked ? 'red' : 'grey'"
+                    @click=like(index,1)>
+                  mdi-heart
+                </v-icon>
               </v-btn>
             </v-card-text>
           </v-card-actions>
@@ -115,6 +130,7 @@ export default {
 }
 </script>
 
-<style>
 
+
+<style scoped>
 </style>
