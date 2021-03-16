285457e338299644ebb87bb0d2c670f7bf14a161
Makisai <jschmaglowski@aol.com>
2021-03-17 00:09:05 +0100

    change hover on landing page

---
+++ Frontend/src/components/photos/GridHover.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-img
+      :src="imageSrc"
+      @mouseenter="imageSrc = imageHover"
+      @mouseleave="imageSrc = imageNormal"
+      :lazy-src="`images/gradient.png`"
+      class="rounded-lg elevation-1 grid-image"
+      aspect-ratio="1">
+  </v-img>
+</template>
+
+<script>
+export default {
+  name: "GridHover",
+  props:{
+    image:{
+      type: String,
+      required: true,
+    },
+    imageHover:{
+      type: String,
+      required: true,
+    },
+    imageNormal:{
+      type: String,
+      required: true,
+    }
+  },
+  data(){
+    return{
+      imageSrc: this.imageNormal
+    }
+}
+}
+</script>
+
+<style scoped>
+
+</style>
