7f5429f356115eb0511290ca307cfaa548bf558e
Mike10101 <mike10101@t-online.de>
2021-03-17 16:53:34 +0100

    improving display of last Photowalk pictures

--- Frontend/src/components/photos/LastPhotowalkGrid.vue
+++ Frontend/src/components/photos/LastPhotowalkGrid.vue
@@ -1,30 +1,32 @@
 <template>
   <v-container>
-    <v-col
-      v-for="(photo, index) in  lastPhotos"
-      :key="index"
-      class="d-flex child-flex"
-      cols="4"
-    >
-      <v-img
-        :src="picture(photo.photo_link)"
-        aspect-ratio="1"
-        class="grey lighten-2"
+    <v-row>
+      <v-col
+        v-for="(photo, index) in lastPhotos"
+        :key="index"
+        class="d-flex child-flex"
+        cols="4"
       >
-        <template v-slot:placeholder>
-          <v-row
-            class="fill-height ma-0"
-            align="center"
-            justify="center"
-          >
-            <v-progress-circular
-              indeterminate
-              color="grey lighten-5"
-            ></v-progress-circular>
-          </v-row>
-        </template>
-      </v-img>
-    </v-col>
+        <v-img
+          :src="picture(photo.photo_link)"
+          aspect-ratio="1"
+          class="grey lighten-2"
+        >
+          <template v-slot:placeholder>
+            <v-row
+              class="fill-height ma-0"
+              align="center"
+              justify="center"
+            >
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -39,8 +41,11 @@ export default {
   computed: {
      lastPhotos(){
       let lastPhotos = [];
-      for (let i = 0; i< 5; i++) {
-          lastPhotos.push(this.$store.state.user.photosLast[i]);  
+      for (let i = 0; i< 6; i++) {
+          if(i < this.$store.state.user.photosLast.length){
+             lastPhotos.push(this.$store.state.user.photosLast[i]);  
+          }
+         
       }
       return lastPhotos;
     },
