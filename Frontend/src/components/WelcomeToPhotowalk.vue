051530abef194bb67f45495ec2680a9f200253a3
Mike10101 <mike10101@t-online.de>
2021-03-17 19:32:09 +0100

    Tutorial Expansion Panel & Carousel are somewhat working

--- Frontend/src/components/WelcomeToPhotowalk.vue
+++ Frontend/src/components/WelcomeToPhotowalk.vue
@@ -6,11 +6,20 @@
         :key="i"
       >
         <v-expansion-panel-header v-text="$t('welcome.step')+' '+(i+1)"> </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <v-img
-          :src="'/images/Tutorial'+(i+1)+'.png'"
-          cols="4">
-          </v-img>
+        <v-expansion-panel-content> 
+          <v-card-text>{{descriptionArray[i]}}</v-card-text>
+          <v-dialog @click:outside="onDialogClose">
+            <template v-slot:activator="{ on, attrs }" >
+              <v-img
+                :src="'/images/Tutorial'+(i+1)+'.png'"
+                @click="dialog = true" v-bind="attrs" v-on="on"
+                >
+              </v-img>
+               </template>
+            <v-card>
+              <TutorialCarousel v-if="dialog" :start-index="index" ></TutorialCarousel>
+            </v-card>
+          </v-dialog>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
@@ -19,12 +28,27 @@
 
 
 <script>
+import TutorialCarousel from "../components/photos/TutorialCarousel"
 export default {
   name: 'WelcomeToPhotowalk',
-
+  components:{TutorialCarousel},
   data(){
     return{
-    
+      dialog: false,
+    }
+  },
+  computed:{
+    descriptionArray(){
+      let description = [];
+      description.push("Entdecke Photowalk");
+      description.push("Hier ist der zweite Schritt");
+      description.push("Und der dritte");
+      description.push("Die Hälfte ist geschafft");
+      description.push("Noch ein kleines bisschen");
+      description.push("Sind das nicht 3 wundervolle Walks?");
+      description.push("Gleich geschafft");
+      description.push("Tutorial bestanden :)");
+      return description
     }
   },
 }
---
+++ Frontend/src/components/photos/TutorialCarousel.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container fluid >
+    <v-carousel hide-delimiters height="90%" v-model="currentIndex">
+      <v-carousel-item
+          v-for="(item,i) in 8"
+          :key="i">
+					<v-card-text>{{descriptionArray[i]}}<!---Description Text here---></v-card-text>
+        <v-img :src="'/images/Tutorial'+(i+1)+'.png'"></v-img>
+      </v-carousel-item>
+    </v-carousel>
+    <v-card-actions style="max-height: 50px">
+      
+    </v-card-actions>
+  </v-container>
+</template>
+
+<script>
+
+
+export default {
+  name: 'TutorialCarousel',
+  props: ['startIndex'],
+  data() {
+    return {
+      photosRearranged: [],
+      currentIndex: 0,
+    }
+  },
+  computed:{
+    descriptionArray(){
+      let description = [];
+      description.push("Entdecke Photowalk");
+      description.push("Hier ist der zweite Schritt");
+      description.push("Und der dritte");
+      description.push("Die Hälfte ist geschafft");
+      description.push("Noch ein kleines bisschen");
+      description.push("Sind das nicht 3 wundervolle Walks?");
+      description.push("Gleich geschafft");
+      description.push("Tutorial bestanden :)");
+      return description
+    }
+  },
+
+  methods: {
+   
+  },
+}
+</script>
