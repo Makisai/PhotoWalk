790d68710adcc0151592def4f04d6b1c12fa5523
Makisai <jschmaglowski@aol.com>
2021-03-19 01:19:56 +0100

    Style Landing Page, Refactor LocalsChange, add LocalsChange to Landing Page

---
+++ Frontend/src/components/photos/ImageGridBottom.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-row>
+    <v-col class="d-flex child-flex" cols="2">
+      <grid-hover
+          :image="`/images/hamburg3.png`"
+          :image-hover="`images/hamburg3_hover.png`"
+      ></grid-hover>
+    </v-col>
+    <v-col class="d-flex child-flex" cols="2">
+      <grid-hover
+          :image="`/images/hamburg5.png`"
+          :image-hover="`images/hamburg5_hover.png`"
+      ></grid-hover>
+    </v-col>
+    <v-col class="d-flex child-flex" cols="2">
+      <grid-hover
+          :image="`/images/hamburg8.png`"
+          :image-hover="`images/hamburg8_hover.png`"
+      ></grid-hover>
+    </v-col>
+    <v-col class="d-flex child-flex" cols="2">
+      <grid-hover
+          :image="`/images/hamburg11.png`"
+          :image-hover="`images/hamburg11_hover.png`"
+      ></grid-hover>
+    </v-col>
+    <v-col class="d-flex child-flex" cols="2">
+      <grid-hover
+          :image="`/images/hamburg12.png`"
+          :image-hover="`images/hamburg12_hover.png`"
+      ></grid-hover>
+    </v-col>
+    <v-col class="d-flex child-flex" cols="2">
+      <grid-hover
+          :image="`/images/hamburg7.png`"
+          :image-hover="`images/hamburg7_hover.png`"
+      ></grid-hover>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import GridHover from "@/components/photos/GridHover";
+export default {
+  name: "ImageGridBottom",
+  components: {GridHover},
+}
+</script>
+
+<style scoped>
+
+</style>
